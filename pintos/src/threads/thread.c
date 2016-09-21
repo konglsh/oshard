89966324187430dfa5d77c13b6ce2d1aba727a15
konglsh <konglsh@kaist.ac.kr>
2016-09-22 04:01:38 +0900

    Update thread.c

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -448,7 +448,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->status = THREAD_BLOCKED;
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
-  t->pp=0
+  t->pp=0;
   t->priority[0]= priority;
   t->magic = THREAD_MAGIC;
 }
