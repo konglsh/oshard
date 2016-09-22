745d7c16629fae529d4136a31a2e0ee4abffe43e
konglsh <konglsh@kaist.ac.kr>
2016-09-22 23:36:07 +0900

    Update synch.c

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -99,6 +99,20 @@ sema_try_down (struct semaphore *sema)
 /* Up or "V" operation on a semaphore.  Increments SEMA's value
    and wakes up one thread of those waiting for SEMA, if any.
    This function may be called from an interrupt handler. */
+int
+find_pp(struct semaphore *sema,struct thread *thread){
+   for(int i=0;i<10;i++){
+      if((thread->donated_list[i].lock)->semaphore == *sema){
+         return i+1;
+      }
+   }
+}
+void
+delete_priority(int *priority, int fp){
+   for(int i=fp; i<9; i++){
+      priority[i]=priority[i+1];
+   }
+}
 void
 sema_up (struct semaphore *sema) 
 {
@@ -108,10 +122,15 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) {
+     int k;
      sort_ready_list(&sema->waiters);
     thread_unblock (list_entry (list_pop_back (&sema->waiters),
                                 struct thread, elem));
-    if(thread_current()->pp>0){thread_current()->pp--;}
+    if(thread_current()->pp>0){
+       k=find_pp(sema,thread_current());
+       delete_priority(thread_current()->priority,k);
+       thread_current()->pp--;
+    }
   }
   sema->value++;
   intr_set_level (old_level);
@@ -204,7 +223,10 @@ lock_acquire (struct lock *lock)
    if(lock->holder!=NULL){
       
       thread_current()->donating = lock->holder;
-      lock->holder->donated= thread_current();
+      struct donated_elem donated;
+      donated.thread =thread_current();
+      donated.lock =lock;
+      lock->holder->donated_list[lock->holder->pp]=donated;
       lock->holder->pp++;
       struct thread *d = thread_current()->donating;
       while(d!=NULL){
