c78c00d28b2c55d878d07fedf7bd740d4fad9488
konglsh <konglsh@kaist.ac.kr>
2016-09-24 04:30:58 +0900

    Update list.h

--- pintos/src/lib/kernel/list.h
+++ pintos/src/lib/kernel/list.h
@@ -160,7 +160,7 @@ void list_insert_ordered (struct list *, struct list_elem *,
                           list_less_func *, void *aux);
 void list_unique (struct list *, struct list *duplicates,
                   list_less_func *, void *aux);
-
+void is_tail(struct list_elem *);
 /* Max and min. */
 struct list_elem *list_max (struct list *, list_less_func *, void *aux);
 struct list_elem *list_min (struct list *, list_less_func *, void *aux);
