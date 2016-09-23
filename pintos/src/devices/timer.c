0f6667f099061ab917836996601c1b6d69ae9f52
konglsh <konglsh@kaist.ac.kr>
2016-09-24 02:48:08 +0900

    Update timer.c

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -19,7 +19,7 @@
 
 /* Number of timer ticks since OS booted. */
 static int64_t ticks;
-static struct list wating_list;
+static struct list waiting_list;
 
 /* Number of loops per timer tick.
    Initialized by timer_calibrate(). */
