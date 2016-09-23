44556b376bf30606e64a3a1a97b29c0c5c57fac5
konglsh <konglsh@kaist.ac.kr>
2016-09-24 02:47:33 +0900

    Update timer.c

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -43,6 +43,8 @@ timer_init (void)
   outb (0x43, 0x34);    /* CW: counter 0, LSB then MSB, mode 2, binary. */
   outb (0x40, count & 0xff);
   outb (0x40, count >> 8);
+  
+  list_init(&waiting_list);
 
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
